cbfd22b909ef684bdd90eaa37ac3f80c04bc6981
Julian Fiander <julian.fiander@mangolanguages.com>
2016-11-18 10:15:15 -0500

    Add default values to Election

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,3 +1,8 @@
 class Election < ApplicationRecord
   has_many :votes
+
+  before_create do
+    self.description = "Test Election #{SecureRandom.hex(12)}" if self.description.blank?
+    self.date = Time.now + 2.months if self.date.blank?
+  end
 end
